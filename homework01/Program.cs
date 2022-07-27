76f0637a79881c5eec0065cf3440ddd47a2d5dc5
Shohjahon Abdumuminov <rakhmatovichsh@gmail.com>
2022-07-27 21:13:08 +0300

    w11

---
+++ homework01/Program.cs
@@ -0,0 +1,12 @@
+﻿int MaxNumber(int a)
+{
+    if (a % 10 > a / 10) 
+    {
+        return a % 10;
+    }
+else
+    {
+        return a / 10;
+    }
+}
+Console.WriteLine(MaxNumber(new Random().Next(10, 100)));
